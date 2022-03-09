d95609d17a5cac59000c3644b9646a127fe9c088
Philipp Serr <episource@gmx.de>
2022-03-09 17:18:41 +0100

    Support arduino-pico core without LittleFS

--- platform.py
+++ platform.py
@@ -14,16 +14,51 @@
 
 import copy
 import platform
+import os
 
 from platformio.managers.platform import PlatformBase
+from platformio.package.meta import PackageSpec
+from platformio.platform.exception import UnknownBoard
 
 
 class RaspberrypiPlatform(PlatformBase):
 
+    __is_pico_core = False
+
     def is_embedded(self):
         return True
+        
+    def get_package(self, name, spec=None):
+        # add "framework-arduino-pico" as alias to "framework-arduinopico"
+        if name == "framework-arduino-pico":
+            name = "framework-arduinopico"
+        if spec and spec.name == "framework-arduino-pico":
+            spec_dict = spec.as_dict()
+            spec_dict.update("name", "framework-arduinopico")
+            spec = PackageSpec(**spec_dict)
+        return self.pm.get_package(spec or self.get_package_spec(name))
 
     def configure_default_packages(self, variables, targets):
+        board = variables.get("board", "pico")
+        build_core = variables.get(
+            "board_build.core", self.board_config(variables.get("board")).get(
+                "build.core", "arduino"))
+        if build_core.lower() == "earlephilhower" or build_core.lower() == "rp2040":
+            build_core = "pico"
+            variables["board_build.core"] = build_core
+            
+            self.__is_pico_core = True
+        
+        if "arduino" in variables.get(
+                "pioframework", []) and build_core != "arduino":
+            core_framework = self.frameworks["arduino-%s" % build_core]
+            core_framework_package = core_framework["package"]
+            default_framework_package = self.frameworks["arduino"]["package"]
+            
+            self.frameworks["arduino"] = core_framework
+            self.packages[core_framework_package]["optional"] = False
+            self.packages[default_framework_package]["optional"] = True
+        
         # configure J-LINK tool
         jlink_conds = [
             "jlink" in variables.get(option, "")
@@ -42,7 +77,23 @@ class RaspberrypiPlatform(PlatformBase):
         return PlatformBase.configure_default_packages(self, variables, targets)
 
     def get_boards(self, id_=None):
-        result = PlatformBase.get_boards(self, id_)
+        try:
+            framework_pico_dir = self.get_package_dir("framework-arduinopico")
+        except KeyError:
+            framework_pico_dir = None
+            
+        if framework_pico_dir and self.__is_pico_core:
+            # framework-arduino-pico name for pico board is rpipico
+            if id_ == "pico":
+                id_ = "rpipico"
+        
+            # load boards from framework second, to overwrite
+            # platform definitions with same name, if any
+            PlatformBase.get_boards(self)
+            result = self._get_boards_pico_dir(framework_pico_dir, id_)
+        else:
+            result = PlatformBase.get_boards(self, id_)
+        
         if not result:
             return result
         if id_:
@@ -51,6 +102,20 @@ class RaspberrypiPlatform(PlatformBase):
             for key, value in result.items():
                 result[key] = self._add_default_debug_tools(result[key])
         return result
+        
+    def _get_boards_pico_dir(self, framework_pico_dir, id_=None):
+        current_boards_dir = self.config.get("platformio", "boards_dir", False)
+        config_parser = self.config._parser
+        if not config_parser.has_section("platformio"):
+            config_parser.add_section("platformio")
+        self.config.set("platformio", "boards_dir",
+            os.path.join(framework_pico_dir, "tools", "json"))
+            
+        try:
+            return PlatformBase.get_boards(self, id_)
+        finally:
+            if current_boards_dir:
+                self.config.set("platformio", "boards_dir", current_boards_dir)
 
     def _add_default_debug_tools(self, board):
         debug = board.manifest.get("debug", {})
